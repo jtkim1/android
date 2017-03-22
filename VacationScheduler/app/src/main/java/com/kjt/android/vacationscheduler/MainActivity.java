b152e59bec4da968c5242b218534479a4cb8aced
jtkim <jtkim@welgate.com>
2017-03-22 14:37:48 +0900

    dialog 띄우기 추가

--- VacationScheduler/app/src/main/java/com/kjt/android/vacationscheduler/MainActivity.java
+++ VacationScheduler/app/src/main/java/com/kjt/android/vacationscheduler/MainActivity.java
@@ -4,43 +4,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     CalendarView calendar;
-    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intent = new Intent(getApplicationContext(),VacationDialogActivity.class);
+
         calendar = (CalendarView) findViewById(R.id.calendarView);
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(),dayOfMonth+"/"+month+"/"+year,Toast.LENGTH_LONG).show();
-
-//                LayoutInflater inflater = getLayoutInflater();
-//                final View dialogView = inflater.inflate(R.layout.vacation_save,null);
-//                AlertDialog.Builder buider= new AlertDialog.Builder(MainActivity.this); //AlertDialog.Builder 객체 생성
-//                buider.setTitle("휴가 저장하기"); //Dialog 제목
-//                buider.setIcon(android.R.drawable.ic_menu_add); //제목옆의 아이콘 이미지(원하는 이미지 설정)
-//                TextView startDay = (TextView)dialogView.findViewById(R.id.vs_vacation_start_value);
-//                startDay.setText(year+"/"+month+"/"+dayOfMonth);
-//
-//                buider.setView(dialogView); //위에서 inflater가 만든 dialogView 객체 세팅 (Customize)
-//                //설정한 값으로 AlertDialog 객체 생성
-//                AlertDialog dialog=buider.create();
-//
-//                //Dialog의 바깥쪽을 터치했을 때 Dialog를 없앨지 설정
-//                dialog.setCanceledOnTouchOutside(false);//없어지지 않도록 설정
-//
-//                //Dialog 보이기
-//                dialog.show();
-
-
+//                Toast.makeText(getApplicationContext(),dayOfMonth+"/"+month+"/"+year,Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(),dialogPopupActivity.class);
+                intent.putExtra("date",year+"년 "+month+"월 "+dayOfMonth+"일");
                 startActivity(intent);
+
+
+                /*LayoutInflater inflater = getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.vacation_save,null);
+                AlertDialog.Builder buider= new AlertDialog.Builder(MainActivity.this); //AlertDialog.Builder 객체 생성
+                buider.setTitle("휴가 저장하기"); //Dialog 제목
+                buider.setIcon(android.R.drawable.ic_menu_add); //제목옆의 아이콘 이미지(원하는 이미지 설정)
+                TextView startDay = (TextView)dialogView.findViewById(R.id.vs_vacation_start_value);
+                startDay.setText(year+"/"+month+"/"+dayOfMonth);
+
+                buider.setView(dialogView); //위에서 inflater가 만든 dialogView 객체 세팅 (Customize)
+                //설정한 값으로 AlertDialog 객체 생성
+                AlertDialog dialog=buider.create();
+
+                //Dialog의 바깥쪽을 터치했을 때 Dialog를 없앨지 설정
+                dialog.setCanceledOnTouchOutside(false);//없어지지 않도록 설정
+
+                //Dialog 보이기
+                dialog.show();*/
+
+
+
 
 
 //                startActivity(new Intent(MainActivity.this, VacationDialogActivity.class));
